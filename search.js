304330b70f998398f3ec751c58e33691d6a7c937
electrosolvez <inamullah.electrosolvez@gmail.com>
2023-10-28 20:52:02 +0500

    a

--- search.js
+++
@@ -1,9 +0,0 @@
-// search.js
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
-const searchResults = document.getElementById('search-results');
-
-searchButton.addEventListener('click', function () {
-    const searchTerm = searchInput.value;
-    alert(`You searched for: ${searchTerm}`);
-});

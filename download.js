887ae36cca69f9a766318bb39f713096ba05f9b9
electrosolvez <inamullah.electrosolvez@gmail.com>
2023-11-05 20:24:55 +0500

    Update download.js

--- download.js
+++ download.js
@@ -1,13 +1,21 @@
 // Function to update the table when changes occur
-    function updateTable() {
-        // Check if the HTML code has changed (you can implement your own logic here)
-        if (/* Add your condition to check if changes occurred */) {
-            // Perform the table update here
-            document.getElementById("last-modification-time-1").textContent = new Date().toLocaleString();
-        }
+function updateTable() {
+    // Check if the HTML code has changed (replace with your own logic)
+    if (checkForChanges()) {
+        // Perform the table update here
+        document.getElementById("last-modification-time-1").textContent = new Date().toLocaleString();
     }
+}
 
-    // Call the updateTable function when the page loads
-    window.onload = function() {
-        updateTable();
-    }
+// Function to check for changes in the HTML code (replace with your own logic)
+function checkForChanges() {
+    // Implement your logic here to detect changes
+    // Return true if changes occurred, otherwise return false
+    return true; // For demonstration, always returning true
+}
+
+// Call the updateTable function when the page loads
+window.onload = function() {
+    updateTable();
+}
+

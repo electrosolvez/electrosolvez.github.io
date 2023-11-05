30b6470165e04c85419224572eba9fe9b0aa7daf
electrosolvez <inamullah.electrosolvez@gmail.com>
2023-11-05 20:35:51 +0500

    Update download.js

--- download.js
+++ download.js
@@ -1,21 +1,13 @@
-// Function to update the table when changes occur
-function updateTable() {
-    // Check if the HTML code has changed (replace with your own logic)
-    if (checkForChanges()) {
-        // Perform the table update here
-        document.getElementById("last-modification-time-1").textContent = new Date().toLocaleString();
-    }
+// Function to update the timestamp
+function updateTimestamp() {
+    const timestampElement = document.getElementById("last-modification-time-1");
+    const currentTimestamp = new Date().toLocaleString();
+    timestampElement.textContent = currentTimestamp;
 }
 
-// Function to check for changes in the HTML code (replace with your own logic)
-function checkForChanges() {
-    // Implement your logic here to detect changes
-    // Return true if changes occurred, otherwise return false
-    return true; // For demonstration, always returning true
-}
-
-// Call the updateTable function when the page loads
+// Call the updateTimestamp function when the page loads
 window.onload = function() {
-    updateTable();
+    updateTimestamp();
 }
+
 
